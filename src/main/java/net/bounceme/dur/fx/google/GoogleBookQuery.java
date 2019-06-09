07a42eba60a4f2ddec743552b06575294bfde0bd
thufir <hawat.thufir@gmail.com>
2019-06-08 21:57:33 -0700

    incremental

--- src/main/java/net/bounceme/dur/fx/google/GoogleBookQuery.java
+++ src/main/java/net/bounceme/dur/fx/google/GoogleBookQuery.java
@@ -1,5 +1,6 @@
 package net.bounceme.dur.fx.google;
 
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class GoogleBookQuery {
@@ -7,5 +8,16 @@ public class GoogleBookQuery {
     private static final Logger log = Logger.getLogger(GoogleBookQuery.class.getName());
 
     public void foo(Properties properties) {
+
+        String key = properties.getProperty("api_key");
+        String originalLanguage = properties.getProperty("original_language");
+        String translatedLanguage = properties.getProperty("translated_language");
+        String text = properties.getProperty("text_to_translate");
+
+        log.info("key\t\t" + key);
+        log.info("from\t\t" + originalLanguage);
+        log.info("to\t\t" + translatedLanguage);
+        log.info("text\t\t" + text);
+
     }
 }
